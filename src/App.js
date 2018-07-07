d6780f0995da9088514ac828f8d122a00d7a09db
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2018-07-08 00:57:23 +0300

    add markups for app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import Header from "./components/Header"
+import Menu from "./components/Menu"
+import Main from "./components/Main"
 
+const url = process.env.PUBLIC_URL + "/img/bg.jpg"
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 21px;
+`
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 131px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 class App extends Component {
   render() {
     return (
-        <div className="wrapper">
-            <SayFullName name="Ирина" surname="Лыпник" link="#"/>
-            <SayFullName name="Сергей" surname="Лыпник" link="#"/>
-            <SayFullName name="Тихон" surname="Лыпник" link="#"/>
+        <div className="App">
+          <HeaderWrapper>
+            <Grid>
+              <Header />
+            </Grid>
+          </HeaderWrapper>
+          <MenuWrapper>
+            <Grid>
+              <Menu />
+            </Grid>
+          </MenuWrapper>
+          <MainWrapper>
+            <Grid>
+              <Main />
+            </Grid>
+          </MainWrapper>
         </div>
     );
   }
 }
 
-function SayFullName(props){
-    return(
-        <div>
-            <h3>Мое имя - {props.name}, моя фамилия - {props.surname} </h3>
-            <a href={props.link}>Ссылка на мой профиль</a>
-        </div>
-    )
-}
+//function SayFullName(props){
+//    return(
+//        <div>
+//            <h3>Мое имя - {props.name}, моя фамилия - {props.surname} </h3>
+//            <a href={props.link}>Ссылка на мой профиль</a>
+//        </div>
+//    )
+//}
 
 
 
